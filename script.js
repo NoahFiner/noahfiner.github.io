e6fc497d8c4c186966ca740ce7b487e4efb2bff0
nfiner <nfiner@umich.edu>
2018-10-09 15:00:18 -0400

    Adjust intro animation, update links for the https switch

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
-// var descTimeout;
-//
-// var Site = function(name, desc, url) {
-//   this.name = name;
-//   this.desc = desc;
-//   this.url = url;
-// };
-//
-// var links = {
-//   multitaskilus: new Site("multitaskilus", "A stress-inducing multitasking game.", "multitaskil.us"),
-//   joanna: new Site("joanna", "An art portfolio for Joanna First.", "joannafirst.com"),
-//   jhumpa: new Site("jhumpa", "A video game for Jhumpa Lahiri's book.", "noahfiner.github.io/Jhumpa-Simulator"),
-//   sqar: new Site("sqar", "A newer, better puzzle edition of sqr.", "noahfiner.github.io/sqar"),
-//   sqr: new Site("sqr", "The earlier, survival edition of sqar.", "noahfiner.github.io/sqar"),
-//   xinabox: new Site("xinabox", "A site for the xinabox Minecraft server", "noahfiner.github.io/xinabox"),
-//   bri: new Site("bri", "A site for Boulder Radiologists, Inc.", "noahfiner.github.io/BRI"),
-//   robotics: new Site("robotics", "A site for Fairview's robotics team.", "fairviewrobotics.com"),
-//   map: new Site("map", "An interactive map of Fairview High School.", "fairviewhs.org/map"),
-//   justgothatway: new Site("justgothatway", "A game where you just go that way.", "justgothatway.co")
-// };
-//
 var height = window.innerHeight;
 var mobileHoverTimeout;
 var mobileHeaderDelay = 50;
-var amountOfIntroSquares = 10;
+var amountOfIntroSquares = 25;
 
 var goToPage = function(page, hash) {
   var urlHash = hash || 0;
@@ -140,12 +119,19 @@ $(document).ready(function() {
   $(window).resize(function() {
     height = window.innerHeight;
   });
-  for(var i = 1; i < 7; i++) {
+  for(var i = 1; i < 4; i++) {
     for(var j = 0; j < amountOfIntroSquares; j++) {
       var elemWidth = Math.floor(Math.random()*150+10);
       var elemHeight = Math.floor(Math.random()*200+10);
+      var randHeight = 1-Math.pow(Math.random(), 1.5);
+      var randWidth = 1-Math.pow(Math.random(), 2);
       var heightMultiplier = 1.2;
-      $("#parallax-back"+i).append("<div style='height: "+elemHeight+"px; width: "+elemWidth+"px; top: "+(Math.floor(Math.random()*(($("#parallax-back"+i).height()*heightMultiplier)))-(elemHeight/2))+"px; left: "+(Math.floor(Math.random()*(($("#parallax-back"+i).width())))-(elemWidth/2))+"px'></div>");
+      console.log(Math.pow(Math.random(), 5));
+      $("#parallax-back"+i).append("<div style='height: "+elemHeight+"px;\
+                width: "+elemWidth+"px;\
+                top: "+randHeight*100+"%;\
+                left: "+randWidth*100+"%'>\
+                </div>");
     }
   }
 });
@@ -157,14 +143,15 @@ $(window).scroll(function() {
     scrollAmt = 0;
   }
   var offset = 100;
-  if(Math.ceil((scroll-offset)/height) <= 1) {
+  var heightsScrolled = Math.ceil((scroll-offset)/height);
+  if(heightsScrolled <= 2) {
+    $("#parallax-back1").css("transform", "translateY(-"+(scroll/0.75)+"px)");
+    $("#parallax-back2").css("transform", "translateY(-"+(scroll/1.0)+"px)");
+    $("#parallax-back3").css("transform", "translateY(-"+(scroll/1.25)+"px)");
+  }
+  if(heightsScrolled <= 1) {
     $("#parallax-background").css("background-image", "url('photography/intro1.jpg')");
   } else {
-    $("#parallax-background").css("background-image", "url('photography/intro"+(Math.ceil((scroll-offset)/height) + 1)+".jpg')");
+    $("#parallax-background").css("background-image", "url('photography/intro"+(heightsScrolled + 1)+".jpg')");
   }
-  $("#parallax-back1").css("transform", "translateY(-"+(scroll/0.5)+"px)");
-  $("#parallax-back2").css("transform", "translateY(-"+(scroll/0.75)+"px)");
-  $("#parallax-back3").css("transform", "translateY(-"+(scroll/1)+"px)");
-  $("#parallax-back4").css("transform", "translateY(-"+(scroll/1.25)+"px)");
-  $("#parallax-back5").css("transform", "translateY(-"+(scroll/1.5)+"px)");
 });
